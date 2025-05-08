37267d8857db09772f35f0dcdc3e7709ab0a14e4
VincentChamberlain2 <s4403904@glos.ac.uk>
2025-05-08 13:09:27 +0100

    More progress towards fixing collision errors

--- Assets/Scripts/Custom Scripts/bladeHit.cs
+++ Assets/Scripts/Custom Scripts/bladeHit.cs
@@ -6,9 +6,12 @@ public class bladeHit : MonoBehaviour
 {
     public GameObject weapon;
 
+    canDrag canDragRef;
+
     void OnCollisionEnter2D(Collision2D collision)
     {
-        weapon.stopDrag();
+        canDragRef = weapon.GetComponent<canDrag>();
+        canDragRef.stopDrag();
     }
 
     void OnCollisionExit2D(Collision2D collision)
